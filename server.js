9fd2991712e2020f59a0340a5d1f2b16712143a8
Ajay120503 <149007089+Ajay120503@users.noreply.github.com>
2024-10-31 23:44:36 +0530

    chat server

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173', // Vite's default dev server port
+        methods: ['GET', 'POST']
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is working!'); // Respond with a message
+});
+
+let userCount = 0;
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    userCount++;
+    io.emit('userCount', userCount);
+
+    // Emit a user joined event
+    socket.broadcast.emit('userJoined', { userId: socket.id });
+
+    socket.on('message', (message) => {
+        io.emit('message', message); // Broadcast the message to all clients
+    });
+
+    socket.on('disconnect', () => {
+        userCount--;
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
